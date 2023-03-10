9c1f4b1df88d4fae3f1083e71edcb65dec743614
abdallah aboodra <abdallahaboodra295@gmail.com>
2023-03-09 23:58:08 -0800

    best seller list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -15,24 +15,53 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        CustomAppBar(),
-        FeaturedBoxListView(),
-        SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 24,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomAppBar(),
+          FeaturedBoxListView(),
+          SizedBox(
+            height: 50,
           ),
-          child: Text(
+          Text(
             'Best Seller',
             style: Styles.titleMedium,
           ),
-        ),
-      ],
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+                decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.red,
+              image: const DecorationImage(
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                  fit: BoxFit.fill),
+            )),
+          ),
+        ],
+      ),
     );
   }
 }
