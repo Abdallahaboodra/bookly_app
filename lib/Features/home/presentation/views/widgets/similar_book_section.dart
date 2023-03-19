303c01efadc79a26243ce65c41c4555d17f676ef
abdallah aboodra <abdallahaboodra295@gmail.com>
2023-03-18 18:21:47 -0700

    refactor book details view body

---
+++ lib/Features/home/presentation/views/widgets/similar_book_section.dart
@@ -0,0 +1,32 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+
+class SimilarBooksSection extends StatelessWidget {
+  const SimilarBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 5,
+          ),
+          child: Text(
+            'You can also like',
+            style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const SimilarBooksListView(),
+      ],
+    );
+  }
+}
