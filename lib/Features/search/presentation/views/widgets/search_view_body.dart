d1610d4145b793da5d64a2ad4448fc0390de1ce5
abdallah aboodra <abdallahaboodra295@gmail.com>
2023-03-21 01:03:07 -0700

    craete custom search text field

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,11 +1,26 @@
+import 'package:bookly_app/constant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kPrimaryPadding,
+      ),
+      child: Column(
+        children: const [
+          CustomSearchTextField(),
+        ],
+      ),
+    );
   }
 }
